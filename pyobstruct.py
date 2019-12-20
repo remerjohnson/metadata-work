8c557fc8f2db18cb8164d1ce4b365c1f3d6fa172
remerjohnson <ryan.emerson.johnson@gmail.com>
2019-12-19 16:09:03 -0800

    Made better comments

--- pyobstruct.py
+++ pyobstruct.py
@@ -3,11 +3,16 @@ import sys
 import os
 import numpy as np
 
-'''The Object Structurer: A script to make DAMS-structured objects from staging folders
+'''
+The Object Structurer: A script to make DAMS-structured objects from staging folders
 
 Step 1: Have the user provide the root directory we want to look at
 
-An example path to try: /mnt/digital-staging/Mexican-Broadsides/batch2/Working_Files'''
+An example path to try on Linux: '/mnt/digital-staging/Mexican-Broadsides/ImageFiles/Working_Files_batch2'
+
+An example path to try on Windows: 'Y:Mexican-Broadsides\ImageFiles\Working_Files_batch2'
+
+'''
 
 user_path = input("Enter the absolute path of the directory containing all objects: ")
 os.chdir(user_path)
@@ -22,21 +27,21 @@ for f in folders:
 
 # Remove any Thumbs.db files in our list
 for file in files:
-    while 'Thumbs.db' in file: file.remove('Thumbs.db')    
-    
+    while 'Thumbs.db' in file: file.remove('Thumbs.db')
+
 # Give the user some textual output of the files, then report total number of objects
 print("Here is a sample of the files:")
-print('\n',files[0:15])
+print('\n', files[0:15])
 print('\n')
-print("There are",len(files),"total objects")
+print("There are", len(files), "total objects")
 
 # Make a dictionary where the key is the bib/folder, and the value is one or more files
 listing_dict = dict(zip(folders, files))
 
-# Make our initial dataframe using the dictionary
+# Make our initial DataFrame using the dictionary
 df = pd.DataFrame.from_dict(listing_dict, orient='index', columns=['file_1', 'file_2'])
 
-# Stack the datafame, reset index
+# Stack the DataFrame, reset index
 df2 = df.stack()
 df2 = df2.reset_index()
 df2.index.name = None
@@ -47,18 +52,18 @@ dupe_df = df2[arks.isin(arks[arks.duplicated()])]
 dupe_arks = dupe_df["level_0"]
 dupe_arks = dupe_arks.unique()
 
-# Make the duplicates dataframe
+# Make the duplicates DataFrame
 dupe_df = pd.DataFrame({'level_0':dupe_arks})
 
-# Concatenate the two dataframes
+# Concatenate the two DataFrame
 final_df = pd.concat([df2, dupe_df], sort=True, ignore_index=True)
 
 # Sort so that object header rows are grouped with components
-final_df = final_df.sort_values(by=['level_0','level_1'], na_position='first')
+final_df = final_df.sort_values(by=['level_0', 'level_1'], na_position='first')
 
-# Give the user a sample of the dataFrame
+# Give the user a sample of the DataFrame
 print(final_df[0:50])
 
-# Print out a csv of the final dataFrame
+# Print out a csv of the final DataFrame to user's Documents directory
 final_df.to_csv('~/Documents/md_structured.csv')
 print("Congratulations! I've output a file in your Documents folder called 'md_structured.csv'")
